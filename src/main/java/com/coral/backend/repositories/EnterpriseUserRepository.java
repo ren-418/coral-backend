a079c672b8ae02560609700580bc9ecfcd5d18c7
ren-418 <mrsteevtvw418@gmail.com>
2024-04-30 12:30:14 -0300

    search enterprise

--- src/main/java/com/coral/backend/repositories/EnterpriseUserRepository.java
+++ src/main/java/com/coral/backend/repositories/EnterpriseUserRepository.java
@@ -9,7 +9,19 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface EnterpriseUserRepository extends JpaRepository<EnterpriseUser, String> {
+  List<EnterpriseUser> findAllByEnterpriseTypeAndAreasAndLocation(String enterpriseType, Area area, String location);
+
+  List<EnterpriseUser> findAllByEnterpriseTypeAndAreas(String enterpriseType, Area area);
+
+  List<EnterpriseUser> findAllByEnterpriseTypeAndLocation(String enterpriseType, String location);
+
+  List<EnterpriseUser> findAllByEnterpriseType(String enterpriseType);
+
   List<EnterpriseUser> findAllByAreasAndLocation(Area area, String location);
+
   List<EnterpriseUser> findAllByAreas(Area area);
+
   List<EnterpriseUser> findAllByLocation(String location);
+
+  List<EnterpriseUser> findAll();
 }
